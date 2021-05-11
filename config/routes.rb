84d906492381eb3be61c9e898944ca40d88aca1e
Mihai Popescu <mihaip@nulogy.com>
2021-05-11 12:40:53 -0400

    Adding scaffolding for a todo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :tasks
+  get 'tags/index'
+  get 'tags/show'
+  get 'tags/create'
+  get 'tags/update'
+  get 'tags/destroy'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/create'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
