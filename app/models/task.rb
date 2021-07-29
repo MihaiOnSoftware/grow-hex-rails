629bb9f961c4fe344064206fb25b842ae54716c1
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2021-07-29 15:38:44 -0400

    Add tag method to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@
 
 class Task < ApplicationRecord
   has_and_belongs_to_many :tags
+
+  def tag(*titles)
+    new_tags = Tag.where(title: titles)
+    tags << new_tags
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Task, type: :model do
 
     expect(Task.first.tags).to include(tag)
   end
+
+  it "can tag with a string" do
+    task = Task.create(title: "A Task")
+    tag = Tag.create(title: "A Tag")
+
+    task.tag(tag.title)
+
+    expect(Task.first.tags).to include(tag)
+  end
+
+  it "can add multiple tags" do
+    task = Task.create(title: "A Task")
+    tag = Tag.create(title: "A Tag")
+    tag2 = Tag.create(title: "Another Tag")
+
+    task.tag(tag.title, tag2.title)
+
+    expect(Task.first.tags).to include(tag, tag2)
+  end
 end
